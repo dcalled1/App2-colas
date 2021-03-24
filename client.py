d162f9d3733af2011064806fd0e5a2af278c18d0
David Calle Daza <dcalled1@eafit.edu.co>
2021-03-23 23:32:57 -0500

    Client done

--- client.py
+++ client.py
@@ -1,27 +1,49 @@
 import requests
+import time
+import random
+import json
 
 
-def prepareTask():
-    pass
+MIN_TIME = 2
+MAX_TIME = 10
+API_HOST = 'http://localhost:3000'
+QUEUE = 'test1'
+
+def prepareTask(id):
+    message = input('Type the description of the task: ')
+    task = {
+        'task_id': id,
+        'username': username,
+        'email': email,
+        'description': message,
+    }
+    time.sleep(random.randint(MIN_TIME, MAX_TIME))
+    return task
 
 def sendTask(task):
-    pass
+    message = json.dumps(task)
+    data = {
+        'username': username,
+        'apiKey': apiKey,
+        'message': message,
+    }
+
+    requests.post("{host}/queues/{queue}/newmessage".format(host=API_HOST, queue=QUEUE), json=data)
 
 
 
 
-API_HOST = 'http://localhost:3000'
 
-
-username = input('type the producer username')
-email = input('type the producer email')
+username = input('type the client username: ')
+email = input('type the client email: ')
 
 
 apiKey = requests.post(API_HOST + '/users/key/new', json={
     'username': username,
     'email': email,
 })
-
+taskCount=0
 while True:
-    task = prepareTask()
+    task = prepareTask(taskCount)
     sendTask(task)
+    taskCount += 1
