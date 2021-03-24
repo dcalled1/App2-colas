6a9e8b9f66a2cb5b22ea2592e0022888bf585dcc
David Calle Daza <dcalled1@eafit.edu.co>
2021-03-24 00:56:14 -0500

    done

--- client.py
+++ client.py
@@ -8,16 +8,19 @@ MIN_TIME = 2
 MAX_TIME = 10
 API_HOST = 'http://localhost:3000'
 QUEUE = 'test1'
+APP_NAME = 'client server'
 
 def prepareTask(id):
-    message = input('Type the description of the task: ')
+    description = input("Type the description of the task_{}: ".format(id))
     task = {
         'task_id': id,
         'username': username,
         'email': email,
-        'description': message,
+        'description': description,
     }
-    time.sleep(random.randint(MIN_TIME, MAX_TIME))
+    prepTime = random.randint(MIN_TIME, MAX_TIME)
+    print("Preparing everything... estimated time: {} seconds".format(prepTime))
+    time.sleep(prepTime)
     return task
 
 def sendTask(task):
@@ -27,8 +30,11 @@ def sendTask(task):
         'apiKey': apiKey,
         'message': message,
     }
+    print("Getting ready to send:\n{}".format(data))
 
     requests.post("{host}/queues/{queue}/newmessage".format(host=API_HOST, queue=QUEUE), json=data)
+
+    print("Done!")
 
 
 
@@ -37,11 +43,20 @@ def sendTask(task):
 username = input('type the client username: ')
 email = input('type the client email: ')
 
+registrationData = requests.post(API_HOST + '/users/new', json={
+    'username': username,
+    'email': email,
+}).json()
+
+print("Registration status:\n", registrationData)
+
 
 apiKey = requests.post(API_HOST + '/users/key/new', json={
     'username': username,
     'email': email,
-})
+}).json()['apiKey']
+
+
 taskCount=0
 while True:
     task = prepareTask(taskCount)
--- server.py
+++ server.py
@@ -1,29 +1,56 @@
 import requests
+import time
+import random
+import json
 
+
+MIN_TIME = 2
+MAX_TIME = 10
+API_HOST = 'http://localhost:3000'
+QUEUE = 'test1'
+APP_NAME = 'client server'
 
 def receiveTask():
-    pass
-
+    print("trying to get tasks...")
+    r = requests.get(url="{host}/queues/{queue}/getmessage".format(host=API_HOST, queue=QUEUE)).json()
+    while not bool(r):
+        r = requests.get(url="{host}/queues/{queue}/getmessage".format(host=API_HOST, queue=QUEUE)).json()
+        time.sleep(1)
+    
+    task = json.loads(r['message'])
+    print("task {id} founded. Description: {desc}".format(id=task['id'], desc=task['description']))
+    
+    return task
 
 def doTask(task):
-    pass
+    taskTime = random.randint(MIN_TIME, MAX_TIME)
+    print("Doing task {id}. Estimated time: {time} seconds.".format(id=task['id'], time=taskTime))
+    time.sleep(taskTime)
 
 
 
 
-API_HOST = 'http://localhost:3000'
 
 
-username = input('type the consumer username')
-email = input('type the consumer email')
+username = input('type the server username: ')
+email = input('type the server email: ')
+
+registrationData = requests.post(API_HOST + '/users/new', json={
+    'username': username,
+    'email': email,
+}).json()
+
+print("Registration status:\n", registrationData)
 
 
 apiKey = requests.post(API_HOST + '/users/key/new', json={
     'username': username,
     'email': email,
-})
+    'appname': APP_NAME,
+}).json()['apiKey']
+
+print("apiKey: {}".format(apiKey))
 
 while True:
     task = receiveTask()
     doTask(task)
-
